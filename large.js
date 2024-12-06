b2bdd39d08322746f2c7e64384669a032291dc83
vidhyashree2004j <vidhyashree2004j@gmail.com>
2024-12-05 23:24:19 -0800

    first commit

---
+++ large.js
@@ -0,0 +1,14 @@
+let a=100
+let b=700
+let c=30
+if(a>b&&a>c)
+{
+    console.log("a is greater")
+}
+else if(b>a&&b>c)
+{
+    console.log("b is greater")
+}
+else{
+    console.log("c is greater")
+}
